779e78a23065e6d76b5bf8c9f5260e3da133fef3
Ave Owl <redowlave@gmail.com>
2016-03-24 17:44:58 +0200

    mutual data config

--- Project/src/main/resources/initial-data.sql
+++ Project/src/main/resources/initial-data.sql
@@ -1,31 +1,33 @@
-/*create table users.Users (
-name CHARACTER VARYING(30),
-salary DOUBLE PRECISION
-);*/
+DROP TABLE iowniwant.user;
+DROP TABLE iowniwant.goals;
 
-/*INSERT INTO users.Users (name, salary) values ('Sam',160.56);
-INSERT INTO users.Users (name, salary) values ('Thomas',93.56);
-INSERT INTO users.Users (name, salary) values ('Amy',75.56);*/
+CREATE TABLE iowniwant.user (
+	user_id					bigserial NOT NULL,
+	first_name			varchar(60) NOT NULL,
+	last_name				varchar(60) NOT NULL,
+	nick_name				varchar(60) NOT NULL UNIQUE,
+	user_password		varchar(60) NOT NULL,
+	email						varchar(60),
+	month_salary		real DEFAULT 0,
+CONSTRAINT user_id_pk PRIMARY KEY (user_id)
+);
 
-/*
-CREATE TABLE users.goals
-(
-  id bigserial NOT NULL,
-  title character varying(255),
-  cost DOUBLE PRECISION,
-  description CHARACTER VARYING(255),
-  pubdate CHARACTER VARYING(255),
-  notes CHARACTER VARYING(255)
-);*/
+CREATE TABLE iowniwant.goals (
+	goals_id 			bigserial  NOT NULL,
+	title 				varchar(255) NOT NULL,
+	cost 					real DEFAULT 0,
+	description 	varchar(255),
+	pubdate 			varchar(255),
+	notes 				varchar(255),
+CONSTRAINT goals_id_pk PRIMARY KEY(goals_id)
+);
 
-INSERT INTO users.goals (title, cost,description,pubdate,notes)
+INSERT INTO iowniwant.goals (title, cost, description, pubdate, notes)
 values ('PC Computer',164.69,'THIS TEXT REPRESENTS AN ARTICLE FOR WHATEVEN YOU DID ADD','22-13-62'
-  ,'Gently my mind escapes into the relaxing mode of pleasure A pleasure that will take my mind off the reality of life ');
-INSERT INTO users.goals (title, cost,description,pubdate,notes)
+	,'Gently my mind escapes into the relaxing mode of pleasure A pleasure that will take my mind off the reality of life ');
+INSERT INTO iowniwant.goals (title, cost, description, pubdate, notes)
 values ('Waschmachine',500.56,'PC TO WORK WHILE YOU RELAX SO WILL IT WORK THIS IS RAP','23-13-62'
-  ,'My past life Life as I know it know And whatever may come it slowly disappears');
-INSERT INTO users.goals (title, cost,description,pubdate,notes)
+	,'My past life Life as I know it know And whatever may come it slowly disappears');
+INSERT INTO iowniwant.goals (title, cost, description, pubdate, notes)
 values ('Vacuum Triode',5124.78,'PC TO WORK WHILE YOU RELAX SO WILL IT WORK THIS IS RAP','24-13-62'
-  ,'to somewhere in the back of my mind It will remain there until I wish to retrieve it ');
-
--- DROP TABLE users.goals;
+	,'to somewhere in the back of my mind It will remain there until I wish to retrieve it ');
