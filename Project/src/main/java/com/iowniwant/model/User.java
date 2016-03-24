8b28923f47e0a8dc475847fa845f0902bcd23a82
RedSulfur <serhiizem@gmail.com>
2016-03-24 16:58:17 +0200

    Providing User class

---
+++ Project/src/main/java/com/iowniwant/model/User.java
@@ -0,0 +1,26 @@
+package com.iowniwant.model;
+
+/**
+ * Created by sulfur on 21.03.16.
+ */
+public class User {
+
+    private String name;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
